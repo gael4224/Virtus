cbb95ef2857b9e5e88a2574f060fca3b8f729451
gael4224 <gaelzinho4334@gmail.com>
2025-11-02 01:41:37 -0600

    Disable React Compiler to fix build

--- snmontery/snmontery/next.config.ts
+++ snmontery/snmontery/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  // reactCompiler: true, // Deshabilitado temporalmente para evitar problemas de build
 };
 
 export default nextConfig;
